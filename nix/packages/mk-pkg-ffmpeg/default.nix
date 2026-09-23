b4aa8c37a1e87699590412fc9c0d26d6047a7bc9
Krishnpal Sendhav <krishnapalsendhav591@gmail.com>
2026-09-24 00:46:52 +0530

    FFmpeg: AES-ECB HLS segments (aes_ecb_key)

--- nix/packages/mk-pkg-ffmpeg/default.nix
+++ nix/packages/mk-pkg-ffmpeg/default.nix
@@ -46,6 +46,7 @@ let
     patch -p1 <${../../../patches/ffmpeg-fix-hls-mp4-seek.patch}
     patch -p1 <${../../../patches/ffmpeg-fix-ios-hdr-texture.patch}
     patch -p1 <${../../../patches/ffmpeg-fix-dash-base-url-escape.patch}
+    patch -p1 <${../../../patches/ffmpeg-aes-ecb.patch}
     cd -
 
     cp ${./meson.build} $src/meson.build
